c13b3a0a2260a9bc018582554949e5826c88fefa
LittleMouse <haozhang9899@gmail.com>
2020-01-27 23:40:14 +0800

    模块化

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,3 @@
-@T
-class User {
+import Util from "./Util";
 
-  constructor(name, age = 18) {
-    this.name = name;
-    this.age = age;
-  }
-
-}
-
-// 用target获取被修饰的类
-function T(target) {
-  console.log(target);
-  target.country = 'China';
-}
-
-console.log(User.country);
+console.log(Util.sum(1, 2));
